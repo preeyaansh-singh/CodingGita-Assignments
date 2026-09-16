ed062a2924bc878e6010c37214ab5a415d942a11
preeyaansh-singh <preeyaansh.singh.cg@gmail.com>
2026-09-16 09:53:46 +0530

    Topic 8 Completed

---
+++ Python-Assignments-CG/Assignment_3_Solution/Topic-8 — Escape Sequences/Solution.py
@@ -0,0 +1,87 @@
+# escape_sequences_answers.py
+# Q42 - Q48 (Escape Sequences)
+
+def q42_new_line():
+    print("Q42 — New Line:")
+    print("Hello\nWorld")
+    print()
+
+
+def q43_tab():
+    print("Q43 — Tab:")
+    print("Name:\tRahul")
+    print("Age:\t20")
+    print("City:\tAhmedabad")
+    print()
+
+
+def q44_backslash():
+    print("Q44 — Backslash:")
+    print("C:\\Python\\Programs")
+    print()
+
+
+def q45_single_quote():
+    print("Q45 — Single Quote:")
+    print('It\'s Python')
+    print()
+
+
+def q46_double_quote():
+    print("Q46 — Double Quote:")
+    print("He said \"Hello\"")
+    print()
+
+
+def q47_predict_output():
+    print("Q47 — Predict Output:")
+    print("Python\nProgramming")
+    print()
+
+
+def q48_combined_escape_sequences():
+    print("Q48 — Combined Escape Sequences:")
+    print("Student Details\n")
+    print("Name:\tRahul")
+    print("Age:\t20")
+    print("Course:\tB.Tech")
+    print()
+
+
+if __name__ == "__main__":
+    q42_new_line()
+    q43_tab()
+    q44_backslash()
+    q45_single_quote()
+    q46_double_quote()
+    q47_predict_output()
+    q48_combined_escape_sequences()
+
+'''Q42:
+Hello
+World
+
+Q43:
+Name:   Rahul
+Age:    20
+City:   Ahmedabad
+
+Q44:
+C:\Python\Programs
+
+Q45:
+It's Python
+
+Q46:
+He said "Hello"
+
+Q47:
+Python
+Programming
+
+Q48:
+Student Details
+
+Name:   Rahul
+Age:    20
+Course: B.Tech'''
