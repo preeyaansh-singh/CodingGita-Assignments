dc012eb2b65e075fb6ba35d6e5c83b62e8803cd3
preeyaansh-singh <preeyaansh.singh.cg@gmail.com>
2026-09-25 19:41:09 +0530

    Topic 1 Completed

---
+++ Python-Assignments-CG/Assignment_4_Solution/Topic-1 — Basic if Statements/Solution.py
@@ -0,0 +1,46 @@
+# Q1. Positive Number
+n = int(input())
+if n > 0:
+    print("Positive Number")
+
+
+# Q2. Voting Eligibility Check
+age = int(input())
+if age >= 18:
+    print("Eligible to Vote")
+
+
+# Q3. Temperature Warning
+temp = int(input())
+if temp > 40:
+    print("High Temperature")
+
+
+# Q4. Divisible by 5
+n = int(input())
+if n % 5 == 0:
+    print("Divisible by 5")
+
+
+# Q5. Free Delivery
+amount = int(input())
+if amount >= 1000:
+    print("Free Delivery")
+
+
+# Q6. Character Check
+ch = input()
+if ch == "A":
+    print("You entered A")
+
+
+# Q7. Password Length Check
+password = input()
+if len(password) >= 8:
+    print("Strong Length")
+
+
+# Q8. Number of Digits
+n = int(input())
+if 100 <= n <= 999:
+    print("Three Digit Number")
